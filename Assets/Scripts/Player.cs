f2e6229a577149256e13fb6dca3948e30f446310
StarGrim33 <prosky33@gmail.com>
2022-11-06 13:45:29 +0300

    2
    Добавил UI

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(Animator))]
 public class Player : MonoBehaviour
@@ -15,7 +16,8 @@ public class Player : MonoBehaviour
     private int _currentHealth;
     private Animator _animator;
     public int Money { get; private set; }
-
+    public event UnityAction<int, int> HealthChanged;
+    public event UnityAction<int> MoneyChanged;
     private void Start()
     {
         _currentWeapon = _weapons[0];
@@ -40,7 +42,7 @@ public class Player : MonoBehaviour
     public void ApplyDamage(int damage)
     {
         _currentHealth -= damage;
-
+        HealthChanged?.Invoke(_currentHealth, _health);
         if (_currentHealth <= 0)
             Destroy(gameObject);
     }
@@ -48,5 +50,14 @@ public class Player : MonoBehaviour
     public void AddMoney(int money)
     {
         Money += money;
+        MoneyChanged?.Invoke(Money);
+
+    }
+
+    public void BuyWeapon(Weapon weapon)
+    {
+        Money -= weapon.Price;
+        MoneyChanged?.Invoke(Money);
+        _weapons.Add(weapon);
     }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -14,6 +14,7 @@ public class Spawner : MonoBehaviour
     private float _timeAfterLastSpawn;
     private int _spawned;
     public event UnityAction AllEnemySpawned;
+    public event UnityAction<int,int> EnemyCountChanged;
 
     private void Start()
     {
@@ -32,6 +33,7 @@ public class Spawner : MonoBehaviour
             InstantiateEnemy();
             _spawned++;
             _timeAfterLastSpawn = 0;
+            EnemyCountChanged?.Invoke(_spawned, _currentWave.Count);
         }
 
         if (_currentWave.Count <= _spawned)
@@ -53,6 +55,7 @@ public class Spawner : MonoBehaviour
     {
         index = Mathf.Min(index, _waves.Count - 1);
         _currentWave = _waves[index];
+        EnemyCountChanged?.Invoke(0, 1);
     }
 
     public void NextWave()
